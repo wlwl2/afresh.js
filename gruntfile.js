774713e7cdb8aec9587b1276c406b666d39441e4
William Lim <manvinedi@gmail.com>
2016-11-19 11:11:20 +0800

    completed tbody and thead example

--- gruntfile.js
+++ gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'sass',
-          src: ['*.scss'],
+          src: ['style.scss'],
           dest: 'css',
           ext: '.css',
           extDot: 'last'
